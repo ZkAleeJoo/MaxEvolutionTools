a6657f76376d21322bda3d29de643f7fcbdba1e6
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-03-08 00:39:17 -0500

    Display ASCII startup banner in console
    Add Bukkit and ChatColor imports and print a multi-line yellow ASCII art banner to the server console during plugin enable. The banner messages are sent via Bukkit.getConsoleSender().sendMessage(...) in MaxEvolutionTools (on enable) to provide startup branding/log output.

--- src/main/java/org/zkaleejoo/MaxEvolutionTools.java
+++ src/main/java/org/zkaleejoo/MaxEvolutionTools.java
@@ -1,5 +1,7 @@
 package org.zkaleejoo;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.zkaleejoo.commands.MainCommand;
 import org.zkaleejoo.config.MainConfigManager;
@@ -14,6 +16,13 @@ public final class MaxEvolutionTools extends JavaPlugin {
     public void onEnable() {
         mainConfigManager = new MainConfigManager(this);
         getCommand("maxevolutiontools").setExecutor(new MainCommand(this));
+
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW+"  _____                 ___________           .__          __  .__            ___________           .__          ");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW+" /     \\ _____  ___  ___\\_   _____/__  ______ |  |  __ ___/  |_|__| ____   ___\\__    ___/___   ____ |  |   ______");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW+" /  \\ /  \\\\__  \\ \\  \\/  / |    __)_\\  \\/ /  _ \\|  | |  |  \\   __\\  |/  _ \\ /    \\|    | /  _ \\ /  _ \\|  |  /  ___/");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW+"/    Y    \\/ __ \\_>    <  |        \\\\   (  <_> )  |_|  |  /|  | |  (  <_> )   |  \\    |(  <_> |  <_> )  |__\\___ \\ ");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW+"\\____|__  (____  /__/\\_ \\/_______  / \\_/ \\____/|____/____/ |__| |__|\\____/|___|  /____| \\____/ \\____/|____/____  >");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW+"        \\/     \\/      \\/        \\/                                            \\/                              \\/ ");
 
     }
 
